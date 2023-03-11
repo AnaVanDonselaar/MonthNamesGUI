4475a279721692eff66581efc408fc6273c46476
Ana Van Donselaar <127625377+AnaVanDonselaar@users.noreply.github.com>
2023-03-11 13:08:01 -0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonthNamesGUI
+{
+    enum Month
+    {
+        Janurary = 1,
+        Feburary,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String input = txtMonth.Text;
+            int monthNum = Convert.ToInt32(input);
+            Month month = (Month)monthNum;
+            txtOutput.Text = month.ToString();
+        }
+
+      
+    }
+}
